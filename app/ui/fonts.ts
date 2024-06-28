2faf08d87485c5d5a445f55ddcae02fa107877dd
Shuo Wang <ws18022@outlook.com>
2024-06-28 17:05:25 +0800

    ch6

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import {Inter, Noto_Sans_SC, Lusitana } from 'next/font/google';
+export const inter = Inter({subsets:['latin']});
+export const notoSanSC = Noto_Sans_SC({subsets:['latin']});
+export const lusitana = Lusitana({subsets:['latin'], weight: ['400', '700']});
